03193c7dbb6db91b059a1d4cddc6710729514bf8
eanzalone <emily.anzalone@gmail.com>
2017-06-15 00:07:48 -0700

    reconfig seed app

--- app/app.js
+++ app/app.js
@@ -1,14 +1,22 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var app = angular.module('myApp', [
+  'ngMaterial',
+  'ngMessages'
+]);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+app.controller('View1Ctrl', function() {
+  var dateObject = {
+    date: moment().format('MMMM Do YYYY'),
+    time: moment().format('h:mm:ss a')
+  };
+  console.log(this);
+  this.testObject = dateObject;
+  // console.log(testery);
+  this.myDate = new Date();
+  this.isOpen = false;
+  $( "#target" ).click(function() {
+    console.log("Handler for .click() called.");
+    console.log(this);
+  });
+});

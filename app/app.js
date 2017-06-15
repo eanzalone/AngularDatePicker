0c0f377aa512b4fdd804f907ed9331380dc06e8b
eanzalone <emily.anzalone@gmail.com>
2017-06-15 02:13:42 -0700

    Can Grab Time and Date

--- app/app.js
+++ app/app.js
@@ -2,21 +2,58 @@
 
 var app = angular.module('myApp', [
   'ngMaterial',
-  'ngMessages'
+  'ngMessages',
+  'mdPickers'
 ]);
 
-app.controller('View1Ctrl', function() {
-  var dateObject = {
+app.controller('ViewCtrl', function() {
+  var dateObjects = [{
     date: moment().format('MMMM Do YYYY'),
     time: moment().format('h:mm:ss a')
-  };
-  console.log(this);
-  this.testObject = dateObject;
+  },
+  {
+    date: moment().format('dddd'),
+    time: moment().format()
+  }];
+  // console.log(this);
+  this.testObject = dateObjects;
   // console.log(testery);
   this.myDate = new Date();
   this.isOpen = false;
   $( "#target" ).click(function() {
     console.log("Handler for .click() called.");
     console.log(this);
+    // debugger;
   });
 });
+
+  
+app.controller("MainCtrl", ['$scope', '$mdpDatePicker', '$mdpTimePicker', function($scope, $mdpDatePicker, $mdpTimePicker){
+  $scope.currentDate = new Date();
+  this.showDatePicker = function(ev) {
+    $mdpDatePicker($scope.currentDate, {
+      targetEvent: ev
+    }).then(function(selectedDate) {
+      // console.log(selectedDate);
+      $scope.selectedDate = selectedDate;
+      // $scope.currentDate = selectedDate;
+    });;
+  };
+  
+  // this.filterDate = function(date) {
+  //   return moment(date).date() % 2 == 0;
+  // };
+  
+  this.showTimePicker = function(ev) {
+    $mdpTimePicker($scope.currentTime, {
+      // debugger;
+      targetEvent: ev
+    }).then(function(selectedDate) {
+      // console.log(selectedDate);
+      console.log($scope.selectedDate);
+      // debugger;
+      $scope.selectedTime = selectedDate;
+      // $scope.currentTime = selectedDate;
+    });;
+  }  
+}]);
